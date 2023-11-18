6264a054791a1448f1ba1ff76cda2928924928bd
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-11-19 05:21:45 +0800

    add ability to specify data dir with env var

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,7 @@
 use crate::APP_NAME;
+use crate::DATA_DIR_ENV_VAR;
+use std::env;
+use std::env::VarError;
 use std::fs;
 use std::io;
 use std::io::ErrorKind;
@@ -11,8 +14,13 @@ pub fn ensure_exists(dir: &Path) -> io::Result<()> {
 	fs::create_dir_all(dir)
 }
 
-pub fn get_data_dir() -> Result<PathBuf, io::Error> {
+pub fn default_location() -> Result<PathBuf, io::Error> {
 	let data_dir = dirs::data_dir()
 		.ok_or_else(|| io::Error::new(ErrorKind::NotFound, "Data directory not found"))?;
 	Ok(data_dir.join(DATA_SUBDIR))
 }
+
+pub fn get_env_location() -> Result<PathBuf, VarError> {
+	Ok(env::var(DATA_DIR_ENV_VAR)?.into())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 use args::Args;
 use clap::Parser;
+use std::env::VarError;
 use std::error::Error;
 use std::path::PathBuf;
 
 const APP_NAME: &str = "clorange";
+const DATA_DIR_ENV_VAR: &str = "CLORANGE_DATA_DIR";
 
 mod args;
 mod data;
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let Args { data } = Args::parse();
+	// I'd love to use more .unwrap_or_else()s here, but ?
 	let data = match data {
 		Some(path) => path,
-		None => data::get_data_dir().map_err(|error| {
-			format!("{}. Specify your own with --data / -d.", error)
-		})?
+		None => {
+			match data::get_env_location() {
+				Ok(path) => path,
+				Err(error) => match error {
+					VarError::NotUnicode(value) => {
+						Err(format!("You specified the {0} environment variable, but it contains invalid unicode.\nValue of {0} specified in backticks: `{1:?}`", DATA_DIR_ENV_VAR, value))?
+					},
+					VarError::NotPresent => data::default_location().map_err(|error| {
+						format!("{}. Specify your own with --data / -d.", error)
+					})?,
+				},
+			}
+		}
 	};
 	data::ensure_exists(&data)?;
 	Ok(())
